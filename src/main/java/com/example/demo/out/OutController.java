dd39ea9def64b2327068c6ae95e8548f32568409
shuolid <ls_2017@163.com>
2021-01-21 14:28:37 +0800

    下单工具优化

--- src/main/java/com/example/demo/out/OutController.java
+++ src/main/java/com/example/demo/out/OutController.java
@@ -3,11 +3,9 @@ package com.example.demo.out;
 import com.example.demo.BizType;
 import com.example.demo.apiModel.BizTypeEnum;
 import com.example.demo.apiModel.ResultModel;
+import com.example.demo.apiModel.SendPayEnum;
 import com.example.demo.service.OutService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +21,16 @@ public class OutController {
     private OutService outService;
 
     @PostMapping("/downLoadShipment")
-    @ApiImplicitParam(name = "routeRule", paramType = "query", allowableValues = "55,98", defaultValue = "55")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "routeRule", paramType = "query", allowableValues = "55,98", defaultValue = "55"),
+        @ApiImplicitParam(name = "sendPay", paramType = "query", allowableValues = "普通C单,直通车订单", defaultValue = "普通C单")
+    })
     @ApiOperation(value = "出库接单", notes = "多商品，多个，多次出库接单")
     public ResultModel downLoadShipment(
             @ApiParam(value = "商品编号",required = true, defaultValue = "100391") @RequestParam(name = "goodId") String goodNo,
             @ApiParam(value = "商品数量",required = true, defaultValue = "1") @RequestParam(name = "goodNum") String goodNum,
             @ApiParam(value = "接口执行次数",required = true, example = "1") @RequestParam(name = "excuteCount") Integer excuteCount,
-            @ApiParam(value = "sendPay",required = true, defaultValue = "1000000020000000000000000200110003300010000000000010000000000103000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000") @RequestParam(name = "sendPay") String sendPay,
+            @RequestParam("sendPay")String sendPay,
             @RequestParam("routeRule")String routeRule){
         BizType bizType = BizType.builder()
                 .bizType(BizTypeEnum.DOWNLOAD_SHIPMENT.getBizType())
@@ -39,7 +40,7 @@ public class OutController {
                 .routeRule(routeRule)
                 .build();
 
-        return outService.downLoadShipment(goodNo, goodNum, excuteCount, sendPay, routeRule, bizType);
+        return outService.downLoadShipment(goodNo, goodNum, excuteCount, SendPayEnum.getEnumByName(sendPay).getValue(), routeRule, bizType);
     }
 
     @PostMapping("/packageSwitch")
